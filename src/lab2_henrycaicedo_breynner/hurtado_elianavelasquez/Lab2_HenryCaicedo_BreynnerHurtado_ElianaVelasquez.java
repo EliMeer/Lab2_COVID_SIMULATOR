9bc04b72488aab823d234a75733f32fea5301494
EliMeer <62387327+EliMeer@users.noreply.github.com>
2020-10-08 15:54:47 -0500

    prueba 2

--- src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez.java
+++ src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez.java
@@ -17,6 +17,7 @@ public class Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez {
     public static void main(String[] args) {
         System.out.println("PULL");
         System.out.println("PUSH 2");
+        System.out.println("PRUEBA SOLICITADA POR HENRRRRY ");
     }
     
 }
