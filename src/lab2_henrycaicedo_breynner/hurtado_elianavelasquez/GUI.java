0ca51546c0da1cee56c3931fe8e9d00b033de2e9
Henry Junior <Henry Junior@LAPTOP-J0ML2P5P>
2020-10-15 00:17:13 -0500

    Clase graphics agregada

--- src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/GUI.java
+++ src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/GUI.java
@@ -8,15 +8,19 @@ package lab2_henrycaicedo_breynner.hurtado_elianavelasquez;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -37,10 +41,11 @@ public class GUI {
     
         //Contenido
         JPanel leftTopPanel = new JPanel();
-        leftTopPanel.setPreferredSize(new Dimension(300, 25));
+        leftTopPanel.setPreferredSize(new Dimension(290, 25));
         leftPanel.add(leftTopPanel, "North");
-        JLabel title = new JLabel("Preferencias");
-        leftTopPanel.add(title);
+        JLabel jlTitle = new JLabel("Propiedades");
+        jlTitle.setFont(new Font("Arial", Font.BOLD, 18));
+        leftTopPanel.add(jlTitle);
 
 
         JPanel content = new JPanel();
@@ -55,9 +60,12 @@ public class GUI {
         JScrollPane leftScroll = new JScrollPane(content);    
         leftPanel.add(leftScroll);
         
-        JPanel panel1 = new JPanel();
+        JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        panel1.setPreferredSize(new Dimension(290, 50));
+
         JLabel jlNumNodes = new JLabel("Número de nodos");
-                 jlNumNodes.setFont(new Font("Arial", Font.PLAIN, 16));
+                 jlNumNodes.setFont(new Font("Arial", Font.PLAIN, 17));
+                 jlNumNodes.setHorizontalAlignment(SwingConstants.RIGHT);
                  panel1.add(jlNumNodes);
                  hGroup.addComponent(panel1);
                  vGroup.addComponent(panel1, GroupLayout.PREFERRED_SIZE,
@@ -74,12 +82,12 @@ public class GUI {
         JPanel rightPanel = new JPanel();
         rightPanel.setPreferredSize(new Dimension(700, 600));
         
-
+        rightPanel.add(new MyGraphics());
         
         
         
-        leftPanel.setBorder(BorderFactory.createLineBorder(Color.red));
-        rightPanel.setBorder(BorderFactory.createLineBorder(Color.green));
+        leftPanel.setBorder(BorderFactory.createLineBorder(Color.gray));
+        rightPanel.setBorder(BorderFactory.createLineBorder(Color.gray));
 
 
 
@@ -87,9 +95,31 @@ public class GUI {
         
         pane.add(leftPanel, "West");
         pane.add(rightPanel, "East");
+        
+        
+        //PRUEBA PINTAR CIRCULOS
+        int[][] m = new int[7][6];
+        m[2][2] = 1;
+        rightPanel.add(new MyGraphics());
+        
            
     }
     
+    public static class MyGraphics extends JComponent {
+
+        private static final long serialVersionUID = 1L;
+
+        MyGraphics() {
+            setPreferredSize(new Dimension(700, 600));
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+       //   g.fillRect(0, 0, 700, 600);
+            g.drawString("Hello",40,40);  
+        }
+    }
     
     public static void showGUI(Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez sim) {
         
