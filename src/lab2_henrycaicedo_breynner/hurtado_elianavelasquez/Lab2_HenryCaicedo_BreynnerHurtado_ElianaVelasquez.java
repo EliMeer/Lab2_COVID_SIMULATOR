769ed5f3b299fff22cc0ed6fa6dc1deba8034eea
Henry Junior <Henry Junior@LAPTOP-J0ML2P5P>
2020-10-16 00:45:18 -0500

    Algunas funciones de prueba

--- src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez.java
+++ src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez.java
@@ -5,6 +5,7 @@
  */
 package lab2_henrycaicedo_breynner.hurtado_elianavelasquez;
 
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.UIManager;
 
 /**
@@ -12,10 +13,12 @@ import javax.swing.UIManager;
  * @author Eliana Velasquez
  */
 public class Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    public Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez() {
+        
+        
+    }
+    
     public static void main(String[] args) {
         
         Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez sim = new Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez();
@@ -34,4 +37,37 @@ public class Lab2_HenryCaicedo_BreynnerHurtado_ElianaVelasquez {
         System.out.println("PRUEBA SOLICITADA POR HENRRRRY ");
     }
     
+    static int numNodosEntero = 7;
+    static int[] listaX = new int[numNodosEntero];
+    int[] listaY = new int[numNodosEntero];
+    
+    public void UbicarNodoEnX(int numNodosEntero){
+        
+    double numNodos = (double)numNodosEntero;
+    double anchoD = numNodos*1.5; 
+    int ancho = (int)anchoD;
+    int alto = numNodosEntero;
+    
+    
+    }
+    
+    public void UbicarNodoEnY(int numNodosEntero){      
+    int alto = numNodosEntero;
+    int randomNum;
+    do{
+        randomNum = ThreadLocalRandom.current().nextInt(0, numNodosEntero + 1);
+    }while(!DisponibleX(randomNum));
+    
+    }
+    
+    public boolean DisponibleX(int num){
+        boolean disponible=false;
+        
+        
+        
+        return disponible;
+    }
+    
 }
+
+
