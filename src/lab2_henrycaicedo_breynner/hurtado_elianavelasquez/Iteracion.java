6c0f8a52dd155d6ad0c5c23937891a28a301180c
Henry Junior <Henry Junior@LAPTOP-J0ML2P5P>
2020-10-18 01:34:51 -0500

    Clase Iteracion creada

---
+++ src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Iteracion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_henrycaicedo_breynner.hurtado_elianavelasquez;
+
+/**
+ *
+ * @author HenryCaicedo
+ */
+public class Iteracion {
+    int num;
+    Iteracion nextIteracion;
+    Nodo firstNodo;
+
+    public Iteracion(int num, Iteracion nextIteracion, Nodo firstNodo) {
+        this.num = num;
+        this.nextIteracion = nextIteracion;
+        this.firstNodo = firstNodo;
+    }
+    
+    
+    
+    
+}
