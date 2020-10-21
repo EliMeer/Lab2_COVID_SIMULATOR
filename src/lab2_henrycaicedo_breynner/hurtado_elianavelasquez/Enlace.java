ae435c18b030b557b786841f25261a8d183d41d6
Henry Junior <Henry Junior@LAPTOP-J0ML2P5P>
2020-10-20 20:20:52 -0500

    Clase creada

---
+++ src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Enlace.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_henrycaicedo_breynner.hurtado_elianavelasquez;
+
+/**
+ *
+ * @author HenryCaicedo
+ */
+public class Enlace {
+    Nodo nodo;
+    Enlace nextEnlace;
+
+
+    public Enlace(Nodo nodo) {
+        this.nodo = nodo;
+    }
+    
+    
+    
+}
