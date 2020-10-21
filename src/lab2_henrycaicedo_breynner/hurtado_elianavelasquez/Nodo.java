6bd89fdd01083cd13cc9da2a9edc0de8e2d4f39d
Henry Junior <Henry Junior@LAPTOP-J0ML2P5P>
2020-10-20 20:20:30 -0500

    Gestión de enlaces agregada

--- src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Nodo.java
+++ src/lab2_henrycaicedo_breynner/hurtado_elianavelasquez/Nodo.java
@@ -19,14 +19,48 @@ public class Nodo extends Ellipse2D.Double{
     boolean infectado;
     int id;
     static int contId=1;
+    Enlace firstEnlace;
+    int getNumEnlaces;
 
-    public Nodo(Nodo nextNodo, boolean tapabocas, boolean infectado, double x, double y, double radio) {
-        super(x, y, radio, radio);
+    public Nodo(Nodo nextNodo, boolean tapabocas, boolean infectado, double x, double y, double diametro) {
+        super(x, y, diametro, diametro);
         this.nextNodo = nextNodo;
         this.tapabocas = tapabocas;
         this.infectado = infectado;
         this.id = contId++;
     }
+
+    public void addEnlace(Nodo q) {
+        Enlace e = new Enlace(q);
+        if(firstEnlace==null){
+            firstEnlace = e;
+        }else{
+           Enlace p = firstEnlace;
+           while(p.nextEnlace != null){
+               p = p.nextEnlace;
+           }
+           p.nextEnlace = e;
+        }
+    }
+    
+    public int getNumEnlaces(){
+        int c=0;
+        if(firstEnlace != null){
+        Enlace p = firstEnlace;
+            do{
+                c++;
+            }while(p!=null);
+            p = p.nextEnlace;
+        }
+        return c;
+    }
+
+    String getId() {
+        return Integer.toString(id) ;
+    }
+    
+    
+    
     
     
 
